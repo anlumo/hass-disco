881633f7b16c21315f6ae40e04d33d5e8fe98ea3
Andreas Monitzer <github@monitzer.com>
2022-07-22 23:48:47 +0200

    Added a binary input to enable/disable the effect

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use hass_rs::client;
 use lazy_static::lazy_static;
-use serde_json::json;
-//use tokio::time::sleep;
-use std::{env::var/*, time::Duration*/};
 use rand::Rng;
+use serde_json::json;
+use std::env::var;
+use tokio::sync::mpsc::{error::TryRecvError, unbounded_channel};
 
 // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkYzQwZjA4NjM0ZjA0NzM1YTY1Y2U2ZTZlYzZlY2JkOCIsImlhdCI6MTY1ODQzNTUxNywiZXhwIjoxOTczNzk1NTE3fQ.Sl9E9yJ_SO5U18VFRmy9clDi5ZF5boYwLvrapBQaF3A
 
 lazy_static! {
-    static ref TOKEN: String = var("HASS_TOKEN").expect("please set up the HASS_TOKEN env variable before running this");
+    static ref TOKEN: String =
+        var("HASS_TOKEN").expect("please set up the HASS_TOKEN env variable before running this");
 }
 
 const ENTITY: [&str; 9] = [
@@ -32,19 +33,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = client::connect("151.217.102.206", 8123, false).await?;
 
     client.auth_with_longlivedtoken(&*TOKEN).await?;
-    log::info!("WebSocket connection and authethication works");
+    log::info!("WebSocket connection and authenthication works");
 
     log::debug!("Get Hass Config");
 
-    loop {
-        let idx = rng.gen_range(0..ENTITY.len());
-        let angle = rng.gen_range(0..360);
-        log::debug!("Setting light {idx} to angle {angle}");
+    let (sender, mut receiver) = unbounded_channel::<bool>();
 
-        client.call_service("light".to_owned(), "turn_on".to_owned(), Some(json!({
-            "entity_id": ENTITY[idx],
-            "hs_color": [angle, 100],
-        }))).await?;
-        // sleep(Duration::from_secs(2)).await;
+    client
+        .subscribe_event("state_changed", move |ws_event| {
+            let data = ws_event.event.data;
+            if data.entity_id == "input_boolean.disco" {
+                if let Some(state) = data.new_state {
+                    sender.send(&state.state == "on").unwrap();
+                }
+            }
+        })
+        .await?;
+
+    loop {
+        let state = receiver.recv().await;
+        if Some(true) == state {
+            loop {
+                let idx = rng.gen_range(0..ENTITY.len());
+                let angle = rng.gen_range(0..360);
+                log::debug!("Setting light {idx} to angle {angle}");
+
+                client
+                    .call_service(
+                        "light".to_owned(),
+                        "turn_on".to_owned(),
+                        Some(json!({
+                            "entity_id": ENTITY[idx],
+                            "hs_color": [angle, 100],
+                        })),
+                    )
+                    .await?;
+
+                match receiver.try_recv() {
+                    Ok(false) => break,
+                    Ok(true) => {}
+                    Err(TryRecvError::Empty) => {}
+                    Err(err) => {
+                        log::error!("Err: {err:?}");
+                        return Err(err.into());
+                    }
+                }
+
+                if Ok(false) == receiver.try_recv() {
+                    break;
+                }
+            }
+        }
     }
 }
